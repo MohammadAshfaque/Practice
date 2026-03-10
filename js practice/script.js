549c8503463b6c8c5328fbea3c14cc787d5746ce
codewithashfaque <mohammadashfaque18@gmail.com>
2026-03-10 23:36:19 +0530

    change color button by add event listener

--- js practice/script.js
+++ js practice/script.js
@@ -1,4 +1,6 @@
 var btn = document.querySelector("button")
 btn.addEventListener("click", function(){
      btn.textContent= "starting";
+     btn.style.backgroundColor= "yellow";
+     btn.style.color= "blue";
 })
