5782db7b6d660f114a18290bf5748f8e452635fb
codewithashfaque <mohammadashfaque18@gmail.com>
2026-03-31 21:18:37 +0530

    feat: adding tooltip

---
+++ dev_prblm5/script.js
@@ -0,0 +1,8 @@
+document.querySelector("#hide")
+.addEventListener("mouseenter", function() {
+    document.querySelector(".tooltip").style.display = "initial";
+    })
+
+    document.querySelector("#hide").addEventListener("mouseleave", function() {
+        document.querySelector(".tooltip").style.display = "none";
+    })
