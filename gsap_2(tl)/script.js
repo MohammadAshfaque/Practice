493a4bdda39f7dcf50c82d444972d9efca1dd478
codewithashfaque <mohammadashfaque18@gmail.com>
2026-04-02 22:19:00 +0530

    learn timeline in gsap

---
+++ gsap_2(tl)/script.js
@@ -0,0 +1,28 @@
+var tl  = gsap.timeline()
+
+tl.to("#box1" , {
+    x:1200,
+    rotate:360,
+    duration:2,
+    rotate:360,
+    scale:0.5,
+    delay:1
+})
+
+
+tl.to("#box2" , {
+    x:1200,
+    rotate:360,
+    duration:1,
+    rotate:360,
+    scale:0.5,
+})
+
+
+tl.to("#box3" , {
+    x:1200,
+    rotate:360,
+    duration:1,
+    rotate:360,
+    scale:0.5,
+})
