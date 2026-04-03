d0ba2a6a3c74b6cb7a7fc50cc56967886907b1aa
codewithashfaque <mohammadashfaque18@gmail.com>
2026-04-03 19:35:31 +0530

    making landing page design using gsap animation

--- gsap1/script.js
+++ gsap1/script.js
@@ -1,13 +1,13 @@
-// gsap.to("#box",{
-//   x:1000,
-//   duration:2,
-//   delay:1,
-// })
-
-gsap.from("#box",{
+gsap.to("#box",{
   x:1000,
   duration:2,
-  rotation:360,
-  backgroundColor:"blue",
   delay:1,
 })
+
+// gsap.from("#box",{
+//   x:1000,
+//   duration:2,
+//   rotation:360,
+//   backgroundColor:"blue",
+//   delay:1,
+// })
